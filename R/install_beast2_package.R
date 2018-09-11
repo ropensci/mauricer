43150f8908f06a5b257ed18caaf7e3969ff6bcdf
richelbilderbeek <richel@richelbilderbeek.nl>
2018-09-11 10:24:42 +0200

    Go Travis

---
+++ R/install_beast2_package.R
@@ -0,0 +1,7 @@
+#' Install a BEAST2 package
+#' @param name the package's name
+#' @return nothing. It does install the BEAST2 package
+install_beast2_package <- function(name) {
+  # STUB: return name
+  name
+}
