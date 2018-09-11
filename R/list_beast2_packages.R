431d99bf189cd728221322bca2698b0f3a4c3d06
richelbilderbeek <richel@richelbilderbeek.nl>
2018-09-11 11:06:29 +0200

    Demo package list

--- R/list_beast2_packages.R
+++ R/list_beast2_packages.R
@@ -1,8 +1,10 @@
+#' List all BEAST2 packages available and installed
+#' @return a data frame
+#' @author Richel J.C. Bilderbeek
 #' @export
 list_beast2_packages <- function()
 {
   # java -cp beast.jar beast.util.PackageManager -list
-
   raw <- system2(
     command = "java",
     args = c(
@@ -13,6 +15,8 @@ list_beast2_packages <- function()
     ),
     stdout = TRUE
   )
+  raw <- raw[c(-1, -2, -4)]
+
   df <- stringr::str_split(raw, pattern = "\\|", n = 5, simplify = TRUE)
   colnames(df) <- c("name", "installed_version", "latest_version", "dependencies", "description")
   df
