411dbbeeb20fe28f9073cbdfe21f4423dc27b83d
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-20 14:45:04 +0200

    Candidate for v0.2

---
+++ NEWS.md
@@ -0,0 +1,39 @@
+# News
+
+Newest versions at top.
+
+## `mauricer` 0.2.0 (2018-10-20)
+
+### NEW FEATURES
+
+  * Works on Windows
+
+### MINOR IMPROVEMENTS
+
+  * None
+
+### BUG FIXES
+
+  * None
+
+### DEPRECATED AND DEFUNCT
+
+  * None
+
+## `mauricer` 0.1.0 (2018-09-11)
+
+### NEW FEATURES
+
+  * First version
+
+### MINOR IMPROVEMENTS
+
+  * None
+
+### BUG FIXES
+
+  * None
+
+### DEPRECATED AND DEFUNCT
+
+  * None
--- vignettes/demo.R
+++ vignettes/demo.R
@@ -22,22 +22,22 @@ print(first_absent_package_name)
 testthat::expect_false(mrc_is_installed(first_absent_package_name))
 
 ## ------------------------------------------------------------------------
-# mrc_install(first_absent_package_name)
+mrc_install(first_absent_package_name)
 
 ## ------------------------------------------------------------------------
-# beast2_packages <- mrc_list() 
-# new_first_absent_package_name <- beast2_packages[ 
-#   beast2_packages$installed_version == "NA", 
-# ]$name[1]
-# expect_true(new_first_absent_package_name != first_absent_package_name)
+beast2_packages <- mrc_list() 
+new_first_absent_package_name <- beast2_packages[ 
+ beast2_packages$installed_version == "NA", 
+]$name[1]
+expect_true(new_first_absent_package_name != first_absent_package_name)
 
 ## ------------------------------------------------------------------------
-# unmrc_install(first_absent_package_name)
+mrc_uninstall(first_absent_package_name)
 
 ## ------------------------------------------------------------------------
-# beast2_packages <- mrc_list() 
-# last_first_absent_package_name <- beast2_packages[ 
-#   beast2_packages$installed_version == "NA", 
-# ]$name[1]
-# expect_true(last_first_absent_package_name == first_absent_package_name)
+beast2_packages <- mrc_list() 
+last_first_absent_package_name <- beast2_packages[ 
+ beast2_packages$installed_version == "NA", 
+]$name[1]
+expect_true(last_first_absent_package_name == first_absent_package_name)
 
