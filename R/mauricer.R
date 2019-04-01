6fba0ef1a6fc9b423cab7c9dfe532a74df7189d7
richelbilderbeek <richel@richelbilderbeek.nl>
2019-04-01 11:40:23 +0200

    Fix doc

---
+++ R/mauricer.R
@@ -0,0 +1,33 @@
+#' mauricer: Install BEAST2 packages.
+#'
+#' 'BEAST2' (<http://www.beast2.org>) is a widely used
+#' Bayesian phylogenetic tool, that uses DNA/RNA/protein data
+#' and many model priors to create a posterior of jointly estimated
+#' phylogenies and parameters.
+#'
+#' 'BEAST2' is commonly accompanied by 'BEAUti 2' (<http://www.beast2.org>),
+#' which, among others, allows one to install 'BEAST2' package.
+#' This package allows to install 'BEAST2' packages from 'R'.
+#'
+#' @seealso
+#' \link{mauricer} is part of the \code{babette} package suite:
+#' \itemize{
+#'   \item{
+#'     \code{babette}: work with BEAST2
+#'   }
+#'   \item{
+#'     \link{beautier}: create BEAST2 input files
+#'   }
+#'   \item{
+#'     \link{beastier}: run BEAST2
+#'   }
+#'   \item{
+#'     \link{mauricer}: install BEAST2 packages
+#'   }
+#'   \item{
+#'     \link{tracerer}: parse and analyse BEAST2 output
+#'   }
+#' }
+#' @docType package
+#' @name mauricer
+NULL
