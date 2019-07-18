fa4589ba9bd109898fb8535c44f784390436fb61
richelbilderbeek <richel@richelbilderbeek.nl>
2019-07-18 14:47:39 +0200

    Fixed test: NS works

--- tests/testthat/test-create_beast2_input_package.R
+++ tests/testthat/test-create_beast2_input_package.R
@@ -15,9 +15,7 @@ test_that("NS, deep", {
     output_filename = output_filename
   )
 
-  # Cannot check a BEAST2 input file to be valid
-  # when it is using a construct of the NS package.
-  # This is the error BEAST2 will give:
+  # If BEAST2 NS package is not installed, this is the error you'll get:
   #
   # "Class could not be found. Did you mean beast.core.util.ESS?"
   # "Perhaps a package required for this class is not installed?"
@@ -27,23 +25,9 @@ test_that("NS, deep", {
   # "      <run id='mcmc' spec='beast.gss.NS'>"                                 # nolint this is commented code indeed
   #
   #
-  # Here, we know that the output_filename holds a valid XML file,
-  # but this is incorrectly flagged as invalid:
-  expect_false(
-    suppressWarnings(
-      beastier::is_beast2_input_file(
-        output_filename
-      )
+  expect_true(
+    beastier::is_beast2_input_file(
+      output_filename
     )
   )
-
-  i_want_to_see_the_beast2_output <- FALSE # nolint indeed a long variable name
-  if (i_want_to_see_the_beast2_output) {
-    suppressWarnings(
-      beastier::is_beast2_input_file(
-        output_filename,
-        verbose = TRUE
-      )
-    )
-  }
 })
