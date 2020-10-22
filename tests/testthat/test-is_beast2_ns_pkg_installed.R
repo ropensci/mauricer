9e5723c5bbee84c1bed8501f2e1951c998c4ca5f
richelbilderbeek <richel@richelbilderbeek.nl>
2020-10-22 14:53:31 +0200

    EOD

--- tests/testthat/test-is_beast2_ns_pkg_installed.R
+++ tests/testthat/test-is_beast2_ns_pkg_installed.R
@@ -1,5 +1,7 @@
 test_that("use", {
   expect_silent(is_beast2_ns_pkg_installed())
+
+  expect_false(is_beast2_ns_pkg_installed(beast2_folder = tempfile()))
 })
 
 test_that("install NS package at non-standard location", {
@@ -8,7 +10,16 @@ test_that("install NS package at non-standard location", {
   if (!curl::has_internet()) return()
 
   beast2_folder <- tempfile()
+  expect_false(is_beast2_ns_pkg_installed(beast2_folder = beast2_folder))
+
+  # Install BEAST2
   beastier::install_beast2(folder_name = beast2_folder)
+  expect_false(
+    is_beast2_pkg_installed(name = "NS", beast2_folder = beast2_folder)
+  )
+  expect_false(is_beast2_ns_pkg_installed(beast2_folder = beast2_folder))
+
+  # Install NS package
   mauricer::install_beast2_pkg(name = "NS", beast2_folder = beast2_folder)
   expect_true(
     is_beast2_pkg_installed(name = "NS", beast2_folder = beast2_folder)
