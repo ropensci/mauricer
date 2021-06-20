22c3f5306289030f6d807f9abc0410f8d9a3d9ba
richelbilderbeek <richel@richelbilderbeek.nl>
2021-06-20 10:45:04 +0200

    Make sure no temp files are left undeleted

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,13 @@
 library(testthat)
 library(mauricer)
 
+# Make sure no temp files are left undeleted
+beautier::clear_beautier_cache()
+beautier::check_empty_beautier_folder()
+beastier::clear_beastier_cache()
+beastier::check_empty_beastier_folder()
+
 test_check("mauricer")
+
+beautier::check_empty_beautier_folder()
+beastier::check_empty_beastier_folder()
