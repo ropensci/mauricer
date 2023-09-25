79ab1149cae306b8bcf9dd568b56ea7989595897
richelbilderbeek <richel@richelbilderbeek.nl>
2023-09-25 19:10:13 +0200

    Fix bash style

--- scripts/build_cran.sh
+++ scripts/build_cran.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+
+rm -rf build
 mkdir build
 
-cp -r ../R build
 cp -r ../inst build
 cp -r ../man build
+# cp -r ../Meta build
+cp -r ../R build
+# cp -r ../src build
 cp -r ../vignettes build
 cp ../DESCRIPTION build
 cp ../NAMESPACE build
 
-cd build
+cd build || exit 42
 R CMD build .
-R CMD check --as-cran $(ls *.tar.gz)
+gz_files=$(ls ./*.tar.gz)
+R CMD check --as-cran "${gz_files}"
